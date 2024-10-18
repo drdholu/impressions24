099b7c11c750a71bfa3ba60e1311c61a430ef450
drdholu <paras.dhole@outlook.com>
2024-10-18 09:49:12 +0530

    added logos and improved carousel for memories

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,7 +7,7 @@ import Memories from './Memories'
 const LandingPage = () => {
   return (
     <div>
-      <div className="flex flex-col items-center justify-center h-screen text-center text-white bg-black">
+      <div className="flex flex-col items-center justify-center h-[90vh] text-center text-white bg-black">
         <img src={ImpressionsLogo} alt="Impressions Logo" className="h-32 mx-auto mb-4 md:h-40" />
         <p className="text-xl">For the artist, by the artist</p>
       </div>
