7fefd6f3f66e2418e33a5915f7eb35440e92401e
drdholu <paras.dhole@outlook.com>
2024-10-19 15:11:43 +0530

    modification for deployment

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ import Navbar from './components/Navbar';
 import BackToTop from "./components/ui/BackToTop"
 
 const App = () => {
-  // const scriptURL1 = 'https://script.google.com/macros/s/AKfycbyYj3vvB0vfW4fhd8LByGqmUJt9PCkTNiZ0QQz4D_6Q_8UuH9dYJKlu6X4W2CqpEy6_mw/exec';
-
-  // const scriptURL2 = 'https://script.google.com/macros/s/AKfycbzaQoSHq-xyF7jiUnI7cPWT2pD-Uibiel0E9dsBM5_4PtEsjNBljLqOgD6ElZXlTawx/exec';
   return (
     <div className='font-sans'>
       <Navbar />
