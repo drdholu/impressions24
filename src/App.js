12030474f791b90c562a2ef0e0a6c5e654b7727c
drdholu <paras.dhole@outlook.com>
2024-10-20 18:06:08 +0530

    suspense loading & removed videos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 // App.js
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import { Routes, Route } from 'react-router-dom';
-import CoordinatorForm from './components/CoordinatorForm';
-import VolunteerForm from './components/VolunteerForm';
-import LandingPage from './components/LandingPage'; // Adjust if this path is incorrect
+// import CoordinatorForm from './components/CoordinatorForm';
+// import VolunteerForm from './components/VolunteerForm';
+// import LandingPage from './components/LandingPage'; // Adjust if this path is incorrect
 import Navbar from './components/Navbar';
 import BackToTop from "./components/ui/BackToTop"
 import Footer from './components/ui/Footer';
+
+const VolunteerForm = lazy(() => import('./components/VolunteerForm'));
+const CoordinatorForm = lazy(() => import('./components/CoordinatorForm'));
+const LandingPage = lazy(() => import('./components/LandingPage'));
 
 const App = () => {
   return (
     <div className='font-sans'>
       <Navbar />
+      <Suspense fallback={<div>Loading...</div>}></Suspense>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/coordinator-form" element={<CoordinatorForm />} />
