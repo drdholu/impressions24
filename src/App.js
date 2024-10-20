da439a22f9f892352dd1a244ec600c1497ab9fc8
drdholu <paras.dhole@outlook.com>
2024-10-20 21:54:05 +0530

    removed suspense loading

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import React, {Suspense, lazy} from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 // import CoordinatorForm from './components/CoordinatorForm';
 // import VolunteerForm from './components/VolunteerForm';
@@ -8,15 +8,14 @@ import Navbar from './components/Navbar';
 import BackToTop from "./components/ui/BackToTop"
 import Footer from './components/ui/Footer';
 
-const VolunteerForm = lazy(() => import('./components/VolunteerForm'));
-const CoordinatorForm = lazy(() => import('./components/CoordinatorForm'));
-const LandingPage = lazy(() => import('./components/LandingPage'));
+import VolunteerForm from './components/VolunteerForm';
+import CoordinatorForm from './components/CoordinatorForm';
+import LandingPage from './components/LandingPage';
 
 const App = () => {
   return (
     <div className='font-sans'>
       <Navbar />
-      <Suspense fallback={<div>Loading...</div>}></Suspense>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/coordinator-form" element={<CoordinatorForm />} />
