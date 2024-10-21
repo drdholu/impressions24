d6db49b93a787b3c5599f653c2fbd8906672fd5d
drdholu <paras.dhole@outlook.com>
2024-10-21 20:12:06 +0530

    removed unused var

--- src/components/CoordinatorForm.js
+++ src/components/CoordinatorForm.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+// import { Link, useNavigate } from 'react-router-dom';
+import {useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AlertCircle, CheckCircle2 } from 'lucide-react';
