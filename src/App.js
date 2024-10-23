320a7d89ef976992d466ea466fdb561dcfd175a7
drdholu <paras.dhole@outlook.com>
2024-10-23 13:09:12 +0530

    added error page

--- src/App.js
+++ src/App.js
@@ -11,13 +11,14 @@ import Footer from './components/ui/Footer';
 // import VolunteerForm from './components/VolunteerForm';
 // import CoordinatorForm from './components/CoordinatorForm';
 import LandingPage from './components/LandingPage';
-
+import ErrorPage from './components/ErrorPage';
 const App = () => {
   return (
     <div className='font-sans'>
       <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="*" element={<ErrorPage />} />
         {/* <Route path="/coordinator-form" element={<CoordinatorForm />} /> */}
         {/* <Route path="/volunteer-form" element={<VolunteerForm />} /> */}
       </Routes>
