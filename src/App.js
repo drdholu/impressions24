d85954c57dc1a88babe6c59f794cd56c767df7bf
drdholu <paras.dhole@outlook.com>
2024-11-24 19:43:46 +0530

    refactor: update App component structure and integrate FiberScene

--- src/App.js
+++ src/App.js
@@ -1,47 +1,61 @@
-// App.js
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-// import CoordinatorForm from './components/CoordinatorForm';
-// import VolunteerForm from './components/VolunteerForm';
-// import LandingPage from './components/LandingPage'; 
-// import Navbar from './components/Navbar';
-import BackToTop from "./components/ui/BackToTop"
-// import Footer from './components/ui/Footer';
+// // App.js
+// import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// // import CoordinatorForm from './components/CoordinatorForm';
+// // import VolunteerForm from './components/VolunteerForm';
+// // import LandingPage from './components/LandingPage'; 
+// // import Navbar from './components/Navbar';
+// import BackToTop from "./components/ui/BackToTop"
+// // import Footer from './components/ui/Footer';
 
-// import VolunteerForm from './components/VolunteerForm';
-// import CoordinatorForm from './components/CoordinatorForm';
-import { urls } from './url';
-import ErrorPage from './components/ErrorPage';
-// import gsap from 'gsap';
-// import Preloader from './components/ui/Preloader';
-import TransitionWrapper from './components/ui/TransitionWrapper';
-import CustomCursor from './components/ui/CustomeCursor';
-// import AnimatedBackground from './components/ui/AnimatedBackground';
-// import Spline from '@splinetool/react-spline';
+// // import VolunteerForm from './components/VolunteerForm';
+// // import CoordinatorForm from './components/CoordinatorForm';
+// import { urls } from './url';
+// import ErrorPage from './components/ErrorPage';
+// // import gsap from 'gsap';
+// // import Preloader from './components/ui/Preloader';
+// import TransitionWrapper from './components/ui/TransitionWrapper';
+// import CustomCursor from './components/ui/CustomeCursor';
+// // import AnimatedBackground from './components/ui/AnimatedBackground';
+// // import Spline from '@splinetool/react-spline';
 
-const App = () => {
+// const App = () => {
+//   return (
+//     // <Preloader>
+//       <div className=''>
+//         {/* <Navbar /> */}
+//         <CustomCursor />
+//        <TransitionWrapper>
+//           <Routes>
+//             {urls.map((url, idx) => {
+//               return (
+//                 <Route key={idx} path={url.url} element={url.element} />
+//               )
+//             })}
+//             <Route path="*" element={<ErrorPage />} />
+//           </Routes> 
+//         </TransitionWrapper>
+//         <BackToTop /> 
+//         {/* <Footer />  */}
+//       </div>
+
+
+//     // </Preloader>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+import FiberScene from './components/FiberScene'
+
+function App() {
   return (
-    // <Preloader>
-      <div className=''>
-        {/* <Navbar /> */}
-        <CustomCursor />
-       <TransitionWrapper>
-          <Routes>
-            {urls.map((url, idx) => {
-              return (
-                <Route key={idx} path={url.url} element={url.element} />
-              )
-            })}
-            <Route path="*" element={<ErrorPage />} />
-          </Routes> 
-        </TransitionWrapper>
-        <BackToTop /> 
-        {/* <Footer />  */}
-      </div>
-
-
-    // </Preloader>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <FiberScene />
+    </div>
   );
-};
+}
 
 export default App;
+
--- src/components/FiberScene.js
+++ src/components/FiberScene.js
@@ -3,21 +3,20 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 
 const Model = () => {
-  const { scene } = useGLTF("public/models/paint kit mini.glb"); // Adjust the path to your GLB file
+  const { scene } = useGLTF(process.env.PUBLIC_URL + "/models/paint kit mini.glb"); // Adjust the path to your GLB file
+  // const { scene } = useGLTF("public/models/paint kit mini.glb");
   return <primitive object={scene} />;
 };
 
-const Viewer = () => {
+const FiberScene = () => {
   return (
-    <div>
-        <Canvas>
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[2, 2, 2]} />
-        <Model />
-        <OrbitControls />
-        </Canvas>
-    </div>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[2, 2, 2]} />
+      <Model />
+      {/* <OrbitControls /> */}
+    </Canvas>
   );
 };
 
-export default Viewer;
+export default FiberScene;
