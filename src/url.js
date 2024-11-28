2ed6c498cf96d1d06e396755b02c8075298e3f19
drdholu <paras.dhole@outlook.com>
2024-11-28 13:53:09 +0530

    Update URL mappings to use Landing component

--- src/url.js
+++ src/url.js
@@ -1,15 +1,12 @@
 // import CoordinatorForm from "./components/CoordinatorForm";
 // import ErrorPage from "./components/ErrorPage";
 // import LandingPage from "./components/LandingPage";
-
 // import CoordinatorForm from "./components/CoordinatorForm";
-import ThreeScene from "./components/landingtrial";
-import App from "./components/fiber";
 // import VolunteerForm from "./components/VolunteerForm";
+import Landing from "./components/Landing";
 
 export const urls = [
-    {url: '/', name: "Home", element: <App />},
-    {url: '/chek', name: "Home", element: <ThreeScene />},
+    {url: '/', name: "Home", element: <Landing />},
     //{url: '/coordinator-form', name: 'Coordinator Forms', element: <CoordinatorForm />},
 
     // {url: '/coordinator-form', name: 'Coordinator Forms', element: <CoordinatorForm />},
