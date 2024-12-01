47d539367fd889af29b36674723616fe6ce83b2b
kvr-996 <157153485+kvr-996@users.noreply.github.com>
2024-12-01 14:29:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
-// App.js
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-// import CoordinatorForm from './components/CoordinatorForm';
-// import VolunteerForm from './components/VolunteerForm';
-// import LandingPage from './components/LandingPage'; 
-// import Navbar from './components/Navbar';
-import BackToTop from "./components/ui/BackToTop"
-// import Footer from './components/ui/Footer';
+import CardComponent from './CardComponent'
+import MainCards from './mainCards'
+import Sponsors from './Sponsors'
 
-// import VolunteerForm from './components/VolunteerForm';
-// import CoordinatorForm from './components/CoordinatorForm';
-import { urls } from './url';
-import ErrorPage from './components/ErrorPage';
-// import gsap from 'gsap';
-// import Preloader from './components/ui/Preloader';
-// import TransitionWrapper from './components/ui/TransitionWrapper';
-import CustomCursor from './components/ui/CustomeCursor';
-// import AnimatedBackground from './components/ui/AnimatedBackground';
-// import Spline from '@splinetool/react-spline';
+function App() {
+  return(
+    <div>
+      <MainCards/>
+    </div>
+  )
+}
 
-const App = () => {
-  return (
-    // <Preloader>
-      <div className=''>
-        {/* <Navbar /> */}
-        <CustomCursor />
-       {/* <TransitionWrapper> */}
-          <Routes>
-            {urls.map((url, idx) => {
-              return (
-                <Route key={idx} path={url.url} element={url.element} />
-              )
-            })}
-            <Route path="*" element={<ErrorPage />} />
-          </Routes> 
-        {/* </TransitionWrapper> */}
-        <BackToTop /> 
-        {/* <Footer />  */}
-      </div>
-
-
-    // </Preloader>
-  );
-};
-
-export default App;
+export default App
