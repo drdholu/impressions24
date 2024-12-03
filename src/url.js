c8894af9e06e9703a9d71bf2942cbf3392c4a62f
DHRUVATOTALA <157183876+DHRUVATOTALA@users.noreply.github.com>
2024-12-03 16:07:27 +0530

    Update url.js

--- src/url.js
+++ src/url.js
@@ -7,10 +7,12 @@ import InProgress from "./components/ui/InProgress";
 // import Landing from "./components/Landing";
 import  Newspaper  from './components/Newspaper';
 import Mmifinal from './components/Mmifinal';
-// import Voting from './components/VotingForm';
+import Voting from './components/VotingForm';
 
 export const urls = [
     { url: '/', name: "Home", element: <Newspaper /> },
+    { url: '/mmi-finalists', name: "mmi-finalists", element: <Mmifinal />, className: "" },
+    { url: '/Voting', name: "mmi-finalists", element: <Voting />, className: "" },
     // { url: '/test', name: "Home", element: <Landing /> },
     { url: '/team', name: "Team", element: <InProgress />, position: [-3, 4, 2], className: "" },
     { url: '/events', name: "Events", element: <InProgress />, position: [-2.5, 3.25, 4], className: "" },
@@ -19,8 +21,7 @@ export const urls = [
     { url: '/proshow', name: "Proshows", element: <InProgress />, position: [0.7, 4, 0], className: "" },
     { url: '/about', name: "About", element: <InProgress />, position: [1.8, 3.45, 0], className: "" },
     { url: '/contact', name: "Contact", element: <InProgress />, position: [2.7, 3.5, 0], className: "" },
-    { url: '/mmi-finalists', name: "mmi-finalists", element: <Mmifinal />, position: [2.7, 3.5, 0], className: "" },
-    // { url: '/Voting', name: "mmi-finalists", element: <Voting />, position: [2.7, 3.5, 0], className: "" },
+    
 
     //{url: '/coordinator-form', name: 'Coordinator Forms', element: <CoordinatorForm />},
 
