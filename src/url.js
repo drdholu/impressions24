fa706264997728c2306cfa3bd60e028a43c0e561
DHRUVATOTALA <157183876+DHRUVATOTALA@users.noreply.github.com>
2024-12-04 18:21:38 +0530

    team

--- src/url.js
+++ src/url.js
@@ -9,11 +9,12 @@ import Landing from "./components/Landing";
 import EventsPage from './components/EventsPage';
 import Explore from './components/Explore';
 import MainCards from './components/EventsModules';
+import Teams from './components/teams';
 
 export const urls = [
     { url: '/', name: "Landing", element: <Landing /> },
     // { url: '/test', name: "Home", element: <Landing /> },
-    // { url: '/team', name: "Team", element: <InProgress />, position: [-3, 4, 2], className: "" },
+    { url: '/team', name: "Team", element: <Teams />, position: [-3, 4, 2], className: "" },
     // { url: '/sponsors', name: "Sponsors", element: <InProgress />, position: [-2.25, 4, 0], className: "" },
     // { url: '/showflow', name: "Showflow", element: <InProgress />, position: [-0.35, 4.25, 0], className: "" },
     // { url: '/proshow', name: "Proshows", element: <InProgress />, position: [0.7, 4, 0], className: "" },
