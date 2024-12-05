8dbdf1eefa04091c9419fa63aed12c7fe067f880
drdholu <paras.dhole@outlook.com>
2024-12-05 17:38:15 +0530

    Refactor: Comment out unused imports and clean up code structure

--- src/components/Modules.js
+++ src/components/Modules.js
@@ -32,33 +32,31 @@
 // export default Modules;
 
 import React from "react";
-import "./Modules.css";
+// import "./Modules.css";
 
 function Modules() {
-    return(
+    return (
         <div>
-            <div className="box-border m-0 p-0 bg-gray-200 overflow-x-hidden font-poppins">
+            <div className="box-border p-0 m-0 overflow-x-hidden bg-gray-200 font-poppins">
                 <section>
-                    {/* <section class="parallax-1"> */}
-                    <section class="parallax-1" className="bg-parallax-1 bg-fixed bg-cover bg-center w-screen min-h-screen flex items-center">
-                        {/* <div class="parallax-inner"> */}
+                    <section class="parallax-1" className="flex items-center w-screen min-h-screen bg-fixed bg-center bg-cover bg-parallax-1">
                         <div class="parallax-inner" className="px-10 py-[10%]">
-                            <h1 className="text-6xl sm:text-7xl lg:text-8xl text-left text-overlay">Music</h1>
-                            <button className="mt-4 px-5 py-2 text-white bg-blue-500 border-none rounded-md hover:bg-blue-700 ml-20">Events</button>
+                            <h1 className="text-6xl text-left sm:text-7xl lg:text-8xl text-overlay">Music</h1>
+                            <button className="px-5 py-2 mt-4 ml-20 text-white bg-blue-500 border-none rounded-md hover:bg-blue-700">Events</button>
                         </div>
-                </section>
-                <section class="parallax-2" className="bg-parallax-2 bg-fixed bg-cover bg-center w-screen min-h-screen flex items-center">
-                    <div className="px-10 py-[10%] text-right sm:text-left">
-                        <h1 className="text-6xl sm:text-7xl lg:text-8xl text-right text-overlay">Dance</h1>
-                        <button className="mt-4 px-5 py-2 text-white bg-blue-500 border-none rounded-md hover:bg-blue-700 lg:ml-[1300px] sm:ml-[0px]">Events</button>
-                    </div>
-                </section>
-                <section class="parallax-3" className="bg-parallax-3 bg-fixed bg-cover bg-center w-screen min-h-screen flex items-center">
-                    <div class="parallax-inner" className="px-10 py-[10%]">
-                        <h1 className="text-6xl sm:text-7xl lg:text-8xl text-left text-overlay">Arts & Craft</h1>
-                        <button className="mt-4 px-5 py-2 text-white bg-blue-500 border-none rounded-md hover:bg-blue-700 ml-20">Events</button>
-                    </div>
-             </section>
+                    </section>
+                    <section class="parallax-2" className="flex items-center w-screen min-h-screen bg-fixed bg-center bg-cover bg-parallax-2">
+                        <div className="px-10 py-[10%] text-right sm:text-left">
+                            <h1 className="text-6xl text-right sm:text-7xl lg:text-8xl text-overlay">Dance</h1>
+                            <button className="mt-4 px-5 py-2 text-white bg-blue-500 border-none rounded-md hover:bg-blue-700 lg:ml-[1300px] sm:ml-[0px]">Events</button>
+                        </div>
+                    </section>
+                    <section class="parallax-3" className="flex items-center w-screen min-h-screen bg-fixed bg-center bg-cover bg-parallax-3">
+                        <div class="parallax-inner" className="px-10 py-[10%]">
+                            <h1 className="text-6xl text-left sm:text-7xl lg:text-8xl text-overlay">Arts & Craft</h1>
+                            <button className="px-5 py-2 mt-4 ml-20 text-white bg-blue-500 border-none rounded-md hover:bg-blue-700">Events</button>
+                        </div>
+                    </section>
                 </section>
             </div>
         </div>
