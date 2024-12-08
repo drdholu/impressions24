214f14af5135845da6c08c43ed7ecf57195fd46d
tanishqtote55 <tanishqtote.55@gmail.com>
2024-12-08 16:59:51 +0530

    Mobile View Update

--- src/components/Modules.js
+++ src/components/Modules.js
@@ -41,7 +41,7 @@ function Modules() {
             <div className="box-border p-0 m-0 overflow-x-hidden bg-gray-200 font-poppins">
                 <section>
                     <section class="parallax-1" className="flex items-center w-screen min-h-screen bg-fixed bg-center bg-cover bg-parallax-1">
-                        <div class="parallax-inner" className="px-10 py-[10%]">
+                        <div class="parallax-inner" className="px-10 py-[10%] text-center sm:text-left">
                             <h1 className="text-6xl sm:text-7xl lg:text-8xl text-left text-overlay text-white">Music</h1>
                             <button className="mt-4 px-6 py-3 text-white bg-blue-500 border-none rounded-md hover:bg-blue-700">
                                 <Link
@@ -54,7 +54,7 @@ function Modules() {
                         </div>
                     </section>
                     <section class="parallax-2" className="bg-parallax-2 bg-fixed bg-cover bg-center w-screen min-h-screen flex items-center">
-                        <div className="px-10 py-[10%] text-right sm:text-left">
+                        <div className="px-10 py-[10%] text-center sm:text-left">
                             <h1 className="text-6xl sm:text-7xl lg:text-8xl text-right text-overlay text-white">Dance</h1>
                             <button className="mt-4 px-6 py-3 text-white bg-blue-500 border-none rounded-md hover:bg-blue-700 lg:ml-[1500px] sm:ml-[0px]">
                                 <Link
@@ -67,7 +67,7 @@ function Modules() {
                         </div>
                     </section>
                     <section class="parallax-3" className="bg-parallax-3 bg-fixed bg-cover bg-center w-screen min-h-screen flex items-center">
-                        <div class="parallax-inner" className="px-10 py-[10%]">
+                        <div class="parallax-inner" className="lg:px-10 px-3 py-[10%] text-center sm:text-left">
                             <h1 className="text-6xl sm:text-7xl lg:text-8xl text-left text-overlay text-white">Arts & Craft</h1>
                             <button className="mt-4 px-6 py-3 text-white bg-blue-500 border-none rounded-md hover:bg-blue-700">
                                 <Link
@@ -80,7 +80,7 @@ function Modules() {
                         </div>
                     </section>
                     <section class="parallax-4" className="bg-parallax-4 bg-fixed bg-cover bg-center w-screen min-h-screen flex items-center">
-                        <div className="px-10 py-[10%] text-right sm:text-left">
+                        <div className="px-10 py-[10%] text-center sm:text-center ">
                             <h1 className="text-6xl sm:text-7xl lg:text-8xl text-right text-overlay text-white">Camera</h1>
                             <button className="mt-4 px-6 py-3 text-white bg-blue-500 border-none rounded-md hover:bg-blue-700 lg:ml-[1500px] sm:ml-[0px]">
                                 <Link
@@ -93,7 +93,7 @@ function Modules() {
                         </div>
                     </section>
                     <section class="parallax-5" className="bg-parallax-5 bg-fixed bg-cover bg-center w-screen min-h-screen flex items-center">
-                        <div class="parallax-inner" className="px-10 py-[10%]">
+                        <div class="parallax-inner" className="px-10 py-[10%] text-center sm:text-left">
                             <h1 className="text-6xl sm:text-7xl lg:text-8xl text-left text-overlay text-white">Shoutout</h1>
                             <button className="mt-4 px-6 py-3 text-white bg-blue-500 border-none rounded-md hover:bg-blue-700">
                                 <Link
@@ -106,7 +106,7 @@ function Modules() {
                         </div>
                     </section>
                     <section class="parallax-6" className="bg-parallax-6 bg-fixed bg-cover bg-center w-screen min-h-screen flex items-center">
-                        <div className="px-10 py-[10%] text-right sm:text-left">
+                        <div className="px-10 py-[10%] text-center sm:text-left">
                             <h1 className="text-6xl sm:text-7xl lg:text-8xl text-right text-overlay text-white">Abhinay</h1>
                             <button className="mt-4 px-6 py-3 text-white bg-blue-500 border-none rounded-md hover:bg-blue-700 lg:ml-[1500px] sm:ml-[0px]">
                                 <Link
