b98014f61dafbf71225ad8e289392939d2f12b98
tanishq-33 <tanishqmotghare33@gmail.com>
2024-12-15 19:58:34 +0530

    Gallery Page Updated

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -13,6 +13,13 @@ const GallerySection = () => {
   return (
     <div className="bg-fixed bg-no-repeat bg-cover bg-galleryBackground w-full h-full">
       <Navbar />
+      <div className="mb-4 mt-12 text-5xl font-extrabold tracking-wider text-center text-white uppercase drop-shadow-md font-paperHeader">
+         <p className="text-[2em]" style={{
+           WebkitTextStroke: "0.01px black",
+           textShadow: "2px 2px 2px black",
+         }}>GALLERY</p>
+       </div>
+      
       <div className="p-10 grid grid-cols-4 grid-rows-3 gap-4 h-screen">
         <div className="relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-96">
           <div className="flex justify-center items-center w-full h-full">
@@ -21,10 +28,10 @@ const GallerySection = () => {
         </div>
         <div className="relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-full">
           <div className="flex justify-center items-center w-full h-full">
-            <img src={images[1]} alt="Gallery Image 2" className="w-full h-full object-cover" />
+            <img src={images[1]} alt="Gallery Image 2" cla  ssName="w-full h-full object-cover" />
           </div>
         </div>
-        <div className="relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-full">
+        <div className="relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-96">
           <div className="flex justify-center items-center w-full h-full">
             <img src={images[2]} alt="Gallery Image 3" className="w-full h-full object-cover" />
           </div>
@@ -34,17 +41,17 @@ const GallerySection = () => {
             <img src={images[3]} alt="Gallery Image 4" className="w-full h-full object-cover" />
           </div>
         </div>
-        <div className="mt-48 relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-48">
+        <div className="mt-48 relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-72 mt-24 pt-2">
           <div className="flex justify-center items-center w-full h-full">
             <img src={images[4]} alt="Gallery Image 5" className="w-full h-full object-cover" />
           </div>
         </div>
-        <div className="relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-full">
+        <div className="relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-96">
           <div className="flex justify-center items-center w-full h-full">
             <img src={images[5]} alt="Gallery Image 6" className="w-full h-full object-cover" />
           </div>
         </div>
-        <div className="relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-full">
+        <div className="mt-3 relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-40">
           <div className="flex justify-center items-center w-full h-full">
             <img src={images[6]} alt="Gallery Image 7" className="w-full h-full object-cover" />
           </div>
@@ -54,17 +61,17 @@ const GallerySection = () => {
             <img src={images[7]} alt="Gallery Image 8" className="w-full h-full object-cover" />
           </div>
         </div>
-        <div className="relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-full">
+        <div className="relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-full mt-32">
           <div className="flex justify-center items-center w-full h-full">
             <img src={images[8]} alt="Gallery Image 9" className="w-full h-full object-cover" />
           </div>
         </div>
-        <div className="relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-full">
-          <div className="flex justify-center items-center w-full h-full">
+        <div className="relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-full mt-28">
+          <div className="flex justify-center items-center w-full h-full ">
             <img src={images[9]} alt="Gallery Image 10" className="w-full h-full object-cover" />
           </div>
         </div>
-        <div className="relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-full">
+        <div className="relative overflow-hidden rounded-lg shadow-lg group bg-white w-full h-96 mt-16">
           <div className="flex justify-center items-center w-full h-full">
             <img src={images[10]} alt="Gallery Image 11" className="w-full h-full object-cover" />
           </div>
